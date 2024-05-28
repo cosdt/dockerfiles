afeac0ae57de28012fcd85ea05a0caa038499a83
Yuanhao Ji <jiyuanhao@apache.org>
2024-05-28 14:42:32 +0800

    wip

--- scripts/install-cann.sh
+++ scripts/install-cann.sh
@@ -22,30 +22,20 @@ esac
 
 CANN_URL_PREFIX="https://ascend-repo.obs.cn-east-2.myhuaweicloud.com/CANN/CANN%20${CANN_VERSION}/"
 CANN_TOOLKIT_FILE=Ascend-cann-toolkit_${CANN_VERSION}_linux-${ARCH}.run
-CANN_KERNELS_FILE=Ascend-cann-kernels-910b_${CANN_VERSION}_linux.run
+CANN_KERNELS_FILE=Ascend-cann-kernels-${CANN_CHIP}_${CANN_VERSION}_linux.run
 CANN_TOOLKIT_URL="${CANN_URL_PREFIX}/${CANN_TOOLKIT_FILE}"
 CANN_KERNELS_URL="${CANN_URL_PREFIX}/${CANN_KERNELS_FILE}"
 
-# Download CANN Toolkit
-wget ${CANN_TOOLKIT_URL} -q -O /tmp/${CANN_TOOLKIT_FILE}
-if [ ! $? -eq 0 ]; then
-  echo "CANN toolkit download failed."
-  exit 1
-fi
-
 # Install CANN Toolkit
-chmod +x /tmp/${CANN_TOOLKIT_FILE}
-/tmp/${CANN_TOOLKIT_FILE} --check --quiet --install --install-for-all --install-path=${CANN_TOOLKIT_HOME}
-
-if [ $? -eq 0 ]; then
-  echo "CANN toolkit installation successful."
-  rm -f /tmp/${CANN_TOOLKIT_FILE}
-else
-  echo "CANN toolkit installation failed."
-  rm -f /tmp/${CANN_TOOLKIT_FILE}
-  exit 1
-fi
+wget ${CANN_TOOLKIT_URL} -q -O /tmp/${CANN_TOOLKIT_FILE}
+bash +x /tmp/${CANN_TOOLKIT_FILE} --check --quiet --install --install-for-all --install-path=${CANN_HOME}
+rm -f /tmp/${CANN_TOOLKIT_FILE}
 
 # Init
-echo "source $CANN_TOOLKIT_HOME/set_env.sh " >> /etc/profile
-source /etc/profile
+# echo "source $CANN_HOME/set_env.sh " >> /etc/profile
+# source /etc/profile
+
+# Install CANN Kernels
+wget ${CANN_KERNELS_URL} -q -O /tmp/${CANN_KERNELS_FILE}
+bash +x /tmp/${CANN_KERNELS_FILE} --check --quiet --install --install-for-all --install-path=${CANN_HOME}
+rm -f /tmp/${CANN_KERNELS_FILE}
