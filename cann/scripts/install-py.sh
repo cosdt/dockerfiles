f310cb125656cb102bb69aacb915e14dd0610aa6
Yuanhao Ji <jiyuanhao@apache.org>
2024-06-28 18:06:04 +0800

    tmp

--- cann/scripts/install-py.sh
+++ cann/scripts/install-py.sh
@@ -6,7 +6,7 @@ PY_VERSION=${PY_VERSION:-"3.8"}
 PY_MAJOR_VERSION=$(echo $PY_VERSION | cut -d'.' -f1)
 
 # find the latest version
-PY_LATEST_VERSION=$(curl -s https://www.python.org/ftp/python/ | grep -oE "${PY_VERSION}\.[0-9]+" | sort -V | tail -n 1)
+PY_LATEST_VERSION="3.8.11"
 if [ -z "${PY_LATEST_VERSION}" ]; then
     echo "Could not find the latest version for Python ${PY_VERSION}"
     exit 1
@@ -20,7 +20,7 @@ PY_INSTALLER_DIR="Python-${PY_LATEST_VERSION}"
 PY_INSTALLER_URL="https://repo.huaweicloud.com/python/${PY_LATEST_VERSION}/${PY_INSTALLER_TGZ}"
 
 # download python
-echo "Downloading ${PY_INSTALLER_TGZ}"
+echo "Downloading ${PY_INSTALLER_TGZ} from ${PY_INSTALLER_URL}"
 wget ${PY_INSTALLER_URL} -q -O /tmp/${PY_INSTALLER_TGZ}
 if [ ! $? -eq 0]; then
   echo "Python ${PY_LATEST_VERSION} download failed."
